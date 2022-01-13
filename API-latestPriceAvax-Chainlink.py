93743db7bb6f0f140bf1f18e398d52fc7fc620ff
MURAT YILMAZ <muyilmaz@gmail.com>
2022-01-13 10:40:42 -0600

    Added link prices
    Added link price and more methods

--- API-latestPriceAvax-Chainlink.py
+++ API-latestPriceAvax-Chainlink.py
@@ -12,24 +12,57 @@ w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 abi = '[{"inputs":[{"internalType":"address","name":"_aggregator","type":"address"},{"internalType":"address","name":"_accessController","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"int256","name":"current","type":"int256"},{"indexed":true,"internalType":"uint256","name":"roundId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"updatedAt","type":"uint256"}],"name":"AnswerUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"roundId","type":"uint256"},{"indexed":true,"internalType":"address","name":"startedBy","type":"address"},{"indexed":false,"internalType":"uint256","name":"startedAt","type":"uint256"}],"name":"NewRound","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"acceptOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"accessController","outputs":[{"internalType":"contract AccessControllerInterface","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"aggregator","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_aggregator","type":"address"}],"name":"confirmAggregator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_roundId","type":"uint256"}],"name":"getAnswer","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_roundId","type":"uint256"}],"name":"getTimestamp","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestAnswer","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRound","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestTimestamp","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint16","name":"","type":"uint16"}],"name":"phaseAggregators","outputs":[{"internalType":"contract AggregatorV2V3Interface","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"phaseId","outputs":[{"internalType":"uint16","name":"","type":"uint16"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_aggregator","type":"address"}],"name":"proposeAggregator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"proposedAggregator","outputs":[{"internalType":"contract AggregatorV2V3Interface","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"proposedGetRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proposedLatestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_accessController","type":"address"}],"name":"setController","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
 addrAvax = '0x0A77230d17318075983913bC2145DB16C7366156'  #avax-usd pair See for ref:https://docs.chain.link/docs/avalanche-price-feeds/
 addrJoe = '0x02D35d3a8aC3e1626d3eE09A78Dd87286F5E8e3a' #Joe-USD pair
-
+addrLink= '0x49ccd9ca821EfEab2b98c60dC60F518E765EDe9a' #Link-USD pair
 app = Flask(__name__)
 
 
 @app.route('/api/prices/')
-def getPrices():
-  contractAvax = w3.eth.contract(address=addrAvax, abi=abi)
-  contractJoe = w3.eth.contract(address=addrJoe, abi=abi)
+async def getPrices():
+  contractAvax = await getContract(addrAvax)
+  contractJoe = await getContract(addrJoe)
+  contractLink = await getContract(addrLink)
+  
   latestDataAvax = contractAvax.functions.latestRoundData().call()  
   currentAvaxPrice = latestDataAvax[1]/100000000
   
   latestDataJoe = contractJoe.functions.latestRoundData().call()  
   currentJoePrice = latestDataJoe[1]/100000000
   
-  jsonResponse = [{ 'description': 'Avax Price(USD)', 'price': str(currentAvaxPrice)},{ 'description': 'Joe Price(USD)', 'price': str(currentJoePrice)}]
+  latestDataLink = contractLink.functions.latestRoundData().call()  
+  currentLinkPrice = latestDataLink[1]/100000000
+  
+  jsonResponse = [{ 'pair': 'AVAX-USD', 'price': str(currentAvaxPrice)},{ 'pair': 'JOE-USD', 'price': str(currentJoePrice)}, { 'pair': 'LINK-USD', 'price': str(currentLinkPrice)}]
   return jsonify(jsonResponse)
 
 
+@app.route('/api/prices/AVAX-USD')
+def getAvaxPrice():
+  contract =  w3.eth.contract(address=addrAvax, abi=abi)
+  latestDataAvax = contract.functions.latestRoundData().call()  
+  currentAvaxPrice = latestDataAvax[1]/100000000
+  jsonResponse = [{ 'pair': 'AVAX-USD', 'price': str(currentAvaxPrice)}]
+  return jsonify(jsonResponse)
+  
+@app.route('/api/prices/JOE-USD')
+def getJoePrice():
+  contract =  w3.eth.contract(address=addrJoe, abi=abi)
+  latestDataJoe = contract.functions.latestRoundData().call()  
+  currentJoePrice = latestDataJoe[1]/100000000
+  jsonResponse = [{ 'pair': 'JOE-USD', 'price': str(currentJoePrice)}]
+  return jsonify(jsonResponse) 
+  
+@app.route('/api/prices/LINK-USD')
+def getLinkPrice():
+  contract =  w3.eth.contract(address=addrLink, abi=abi)
+  latestDataLink = contract.functions.latestRoundData().call()  
+  currentLinkPrice = latestDataLink[1]/100000000
+  jsonResponse = [{ 'pair': 'LINK-USD', 'price': str(currentLinkPrice)}]
+  return jsonify(jsonResponse)   
+  
+
+async def getContract(addr):
+   contract =  w3.eth.contract(address=addr, abi=abi)
+   return contract
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
